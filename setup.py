5ffe3aaa774353041462f494be1a977db672865f
Manoj Jahgirdar <manoj.jahgirdar@in.ibm.com>
2024-09-20 12:21:25 +0530

    bug fixes

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="docfusion",
-    version="0.1.0",
+    version="0.1.2",
     author="Manoj Jahgirdar",
     author_email="manoj.jahgirdar@in.ibm.com",
     description="Doc Fusion is a Data Sourcing framework capable of parsing various data types such as pdf, txt, md, docx, xlsx, csv and even a webpage url.",
-    long_description="Doc Fusion is a Data Sourcing framework capable of parsing various data types such as pdf, txt, md, docx, xlsx, csv and even a webpage url. It can handle several types of data such as multi columnar, tabular and invoices. The framework uses an LLM (Large Language Model) Agentic approach, where each data type is managed by a dedicated LLM Agent.",
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/IBM/doc-fusion",
     packages=find_packages(where="src"),
@@ -14,9 +14,14 @@ setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",  # Replace with your license
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.9',
-    install_requires=open("requirements.txt").read().splitlines(),  # Use requirements.txt for dependencies
+    install_requires=open("requirements.txt").read().splitlines(),
+    entry_points={
+        'console_scripts': [
+            'docfusion=main:main',
+        ],
+    },
 )
